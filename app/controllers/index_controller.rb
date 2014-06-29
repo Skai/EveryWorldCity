17d1af7665557a856247335e2360ec49e9801429
Alexey Kovtunets <climbonn@gmail.com>
2014-06-29 23:56:10 +0200

    Added defoult scope for the City model. Logic for showing random city if no params.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,10 +2,12 @@ class IndexController < ApplicationController
   include IndexHelper
   
   def show
-    #@city = City.find(params[:id])
-
-    @city = City.find_by_friendly_url(params[:id])
-
-    p @rectangle_coordinates = get_rectangle_coordinates(@city.latitude, @city.longitude, 5).to_json
+    if params[:id].nil?
+      ids = City.pluck(:id)
+      @city = City.find(ids.sample)
+    else
+      @city = City.find_by_friendly_url(params[:id])
+    end
+    @rectangle_coordinates = get_rectangle_coordinates(@city.latitude, @city.longitude, 5).to_json
   end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,6 @@
 class City < ActiveRecord::Base
+  default_scope -> { where.not(:wiki_page_id => nil) }
+  default_scope -> { where.not(:wiki_page_id => -1) }
   def to_param
     "#{self.friendly_url}"
   end
