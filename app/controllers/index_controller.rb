cfc65cf3a82de0ecf01111cc3c1817f14e750d10
Alexey Kovtunets <climbonn@gmail.com>
2014-07-10 16:20:41 +0200

    Changed getting of random city.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,14 +2,13 @@ class IndexController < ApplicationController
   include IndexHelper
   
   def show
+    #show random city if homepage.
     if params[:id].nil?
-      ids = City.pluck(:id)
-      @city = City.find(ids.sample)
+      @city = City.limit(1).order('RANDOM()').first
     else
       @city = City.find_by_friendly_url(params[:id])
-    end 
+    end
     raise "I don't know this country or city!" if @city.nil?
-    #@countries = City.pluck(:country).uniq
     @countries = City.pluck('DISTINCT country').sort
     @rectangle_coordinates = get_rectangle_coordinates(@city.latitude, @city.longitude, 5).to_json
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   #resources :cities
   #get '/:id/' => "index#show"
-  #get "/pairings/:id/:token/print_email" => "pairings#print_email", as: :print_email
-  #get "/:id/" => "root_controller#display"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
