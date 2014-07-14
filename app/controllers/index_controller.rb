5790ce463b39ef11532f37a337dd15fbc2689c03
Alexey Kovtunets <climbonn@gmail.com>
2014-07-15 01:07:26 +0200

    Changed exceprion to RecordNotFound.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -8,7 +8,8 @@ class IndexController < ApplicationController
     else
       @city = City.find_by_friendly_url(params[:id])
     end
-    raise "I don't know this country or city!" if @city.nil?
+    raise ActiveRecord::RecordNotFound if @city.nil?
+
     @countries = City.pluck('DISTINCT country').sort
     @rectangle_coordinates = get_rectangle_coordinates(@city.latitude, @city.longitude, 5).to_json
   end
@@ -16,4 +17,6 @@ class IndexController < ApplicationController
   def get_cities
     render json: City.select(:id, :city, :friendly_url).where(:country => params[:country])
   end
+
+  
 end
