b2976e9338bb05e43688b4b503fb1782f2366d7f
Elena Kovtunets <mal.slider@gmail.com>
2014-08-01 01:30:15 +0200

    initial html + css

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -36,4 +36,78 @@ $(document).on('page:change', function() {
   select_city  = $select_city[0].selectize;
   select_country = $select_country[0].selectize;
   select_city.disable();
+
+  $('.open-qbox').click(function(e){
+    e.preventDefault();
+    $('#footer').toggleClass('active');
+  });
+
+  // scrolling Page to top with a lot of content
+  $('#nav a').click(function(){
+    var goal = $(this).attr('href');
+    $("#nav li.active").removeClass("active");
+    $(this).parent('li').addClass('active')
+    scrollPage(goal);
+    return false;
+  });
+
+  function scrollPage(id){
+    $('html, body').animate({
+      scrollTop: $(id).offset().top      
+     }, 1000);    
+  };
+
+  // highlightCurrentItem active nav item 
+  function highlightCurrentItem() {
+    var h = $(".section:first").height();
+    var sIndex = Math.floor($(window).scrollTop() / h);
+    var $sItem = $("#nav li").eq(sIndex);
+    if (!$sItem.hasClass("active")) {
+        $("#nav li.active").removeClass("active");
+        $sItem.addClass("active");
+    }    
+  };
+
+  highlightCurrentItem();
+
+  $(window).scroll(function(e) {
+      //highlightCurrentItem();
+  });   
+
+  /* calculate height of section */
+  function calcHeight(){
+    var _height = $(window).height();
+    $(".section").css( "height", _height);
+  };
+  calcHeight();
+
+  $(window).resize(calcHeight);
+
+  /* sticky header */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100){  
+        $('#header').addClass("sticky");
+      }
+      else{
+        $('#header').removeClass("sticky");
+      }
+  });
+
+  $('#toggle-search').on('click', function(){
+    $('#header').toggleClass('active');
+  });
+      
+
+  /* google maps */
+  function initialize() {
+    var map_canvas = document.getElementById('map_canvas');
+    var map_options = {
+      center: new google.maps.LatLng(44.5403, -78.5463),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(map_canvas, map_options)
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 });
