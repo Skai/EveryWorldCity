a54e2ff037e4aefb176409ff20e501de49b5023e
Alexey Kovtunets <climbonn@gmail.com>
2014-08-13 00:43:22 +0200

    Added default finder for admin city - find_by_friendly_url.

--- app/admin/city.rb
+++ app/admin/city.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register City do
 
-
+  controller do
+    defaults finder: :find_by_friendly_url
+  end
+  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
